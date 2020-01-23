58f0e850b9d7be57f8ebd6a694defe1dae1ede5c
Xander-C <1019104544@qq.com>
2020-01-23 23:55:45 +0800

    js...

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 import { requestWeatherDays, requestWeatherHours, requestWeatherAlarm, requestWeatherTips } from './shared/api.js';
+import { createElement, createAnimation } from './shared/util.js';
+const bodyElement = document.querySelector("body");
 const searchElement = document.querySelector("#input");
 const nowCityElement = document.querySelector("#now-city");
 const nowTempElement = document.querySelector("#now-temp");
@@ -10,18 +12,21 @@ document.querySelector("#search-city").addEventListener("click", handleFetchWeat
 var tipsData;
 handleFetchWeather();
 let i = "0";
-async function changeTips(){
+function changeTips(){
     if(i == "0") i = "1";
     else i = "0";
     tipsElement.innerHTML = tipsData.data.tips.observe[i];
 }
 async function handleFetchWeather() {
+    bodyElement.classList.remove("flash-infinite");
+    bodyElement.classList.add("flash-infinite");
     const str = searchElement.value;
     const location = str.split(" ");
     console.log(location[0],location[1]);
     nowCityElement.innerHTML = str;
     const daysData = await requestWeatherDays(location[0], location[1]);
     if(!daysData.data.forecast_24h["0"]){
+        bodyElement.classList.remove("flash-infinite");
         alert("错误的城市");
         return;
     }
@@ -36,4 +41,5 @@ async function handleFetchWeather() {
     nowTempElement.innerHTML = hoursData.data.forecast_1h["0"].degree + "°";
     nowWeatherElement.innerHTML = hoursData.data.forecast_1h["0"].weather;
     WindElement.innerHTML = hoursData.data.forecast_1h["0"].wind_direction + "&nbsp;" + hoursData.data.forecast_1h["0"].wind_power + "级";
+    bodyElement.classList.remove("flash-infinite");
 }
