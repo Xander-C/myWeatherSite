4cd8a44eddd616d20292a641fa0375451478ff3d
Xander_C <1019104544@qq.com>
2020-01-23 11:57:59 +0800

    change the weather

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
-import { requestWeatherDays } from './shared/api.js';
+import { requestWeatherDays, requestWeatherHours } from './shared/api.js';
 const searchElement = document.querySelector("#input");
 const nowCityElement = document.querySelector("#now-city");
+const nowTempElement = document.querySelector("#now-temp");
+const nowWeatherElement = document.querySelector("#now-weather");
 document.querySelector("#search-city").addEventListener("click", handleFetchWeather);
 async function handleFetchWeather() {
-    let str = searchElement.value;
-    let location = str.split(" ");
+    const str = searchElement.value;
+    const location = str.split(" ");
+    console.log(location[0],location[1]);
     nowCityElement.innerHTML = str;
-    let days = requestWeatherDays(location[0], location[1]);
-
+    const daysData = await requestWeatherDays(location[0], location[1]);
+    if(!daysData.data.forecast_24h["0"]){
+        alert("错误的城市");
+        return;
+    }
+    console.log(daysData);
+    const hoursData = await requestWeatherHours(location[0],location[1]);
+    console.log(hoursData);
+    nowTempElement.innerHTML = hoursData.data.forecast_1h["0"].degree+"°";
+    nowWeatherElement.innerHTML = hoursData.data.forecast_1h["0"].weather;
 }
