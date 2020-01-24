1ec4b4a0fcc4fcd116f68b03923a09708ecac2ff
Xander-C <1019104544@qq.com>
2020-01-24 11:54:37 +0800

    aa

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-import { requestWeatherDays, requestWeatherHours, requestWeatherAlarm, requestWeatherTips } from './shared/api.js';
-import { createElement, createAnimation } from './shared/util.js';
+import { requestWeatherData } from './shared/api.js';
 const bodyElement = document.querySelector("body");
 const searchElement = document.querySelector("#input");
 const nowCityElement = document.querySelector("#now-city");
 const nowTempElement = document.querySelector("#now-temp");
 const nowWeatherElement = document.querySelector("#now-weather");
 const WindElement = document.querySelector("#wind");
-const tipsElement = document.querySelector("#tip")
+const tipsElement = document.querySelector("#tip");
+const hoursElement = document.querySelector("#hours");
 document.querySelector("#tips-key").addEventListener("click", changeTips);
 document.querySelector("#search-city").addEventListener("click", handleFetchWeather);
 var tipsData;
@@ -24,22 +24,28 @@ async function handleFetchWeather() {
     const location = str.split(" ");
     console.log(location[0],location[1]);
     nowCityElement.innerHTML = str;
-    const daysData = await requestWeatherDays(location[0], location[1]);
+    const daysData = await requestWeatherData("forecast_24h", location[0], location[1], location[2]);
     if(!daysData.data.forecast_24h["0"]){
         bodyElement.classList.remove("flash-infinite");
         alert("错误的城市");
         return;
     }
     console.log(daysData);
-    const hoursData = await requestWeatherHours(location[0],location[1]);
-    console.log(hoursData);
-    const alarmData = await requestWeatherAlarm(location[0],location[1]);
-    console.log(alarmData);
-    tipsData = await requestWeatherTips(location[0],location[1]);
+    tipsData = await requestWeatherData("tips", location[0], location[1], location[2]);
     console.log(tipsData);
+    const hoursData = await requestWeatherData("forecast_1h", location[0], location[1], location[2]);
+    console.log(hoursData);
 
     nowTempElement.innerHTML = hoursData.data.forecast_1h["0"].degree + "°";
     nowWeatherElement.innerHTML = hoursData.data.forecast_1h["0"].weather;
     WindElement.innerHTML = hoursData.data.forecast_1h["0"].wind_direction + "&nbsp;" + hoursData.data.forecast_1h["0"].wind_power + "级";
+    for (var i = 0; i < 12; i++) {
+        // let pointer = hoursElement.firstChild;
+        const timeIndex = (i) => `#${i}time`
+        const dataIndex = (i) => `${i}`
+        let time = hoursData.data.forecast_1h[dataIndex(i)].update_time.slice(7, 9);
+        // pointer = pointer.firstChild;
+        document.querySelector("#1time").innerHTML = time + ":00";
+    }
     bodyElement.classList.remove("flash-infinite");
 }
